efd678726ed0e011cfef5917ff8a385950dbccf8
Sanna Lun <tin.lun@aalto.fi>
2023-06-28 14:42:06 +0300

    torchrun with 4 gpus

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=4
+#SBATCH -p gpu-nvlink
 #SBATCH --time=01:00:00
-#SBATCH -J digitala_fi
-#SBATCH --mem=20G
+#SBATCH --job-name=digitala_fi
+#SBATCH --mem=10G
 #SBATCH --output=output.out
 #SBATCH --error=errors.err
 
 module load anaconda
+module load cuda 
 source activate w2v2
 
-srun torchrun finetune.py --lang=fi
+export CUDA_DEVICE_ORDER=FASTEST_FIRST
+torchrun --nproc_per_node=4 finetune.py --lang=fi
 
 # watch -n 5 nvidia-smi >> gpu_usage.txt
 # srun python -u finetune.py --lang=fi
