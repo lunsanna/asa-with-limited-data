a83745b6cfd215c0871e4ff8d610958a7d67be87
Sanna Lun <tin.lun@aalto.fi>
2023-07-03 15:21:18 +0300

    lastest run

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH -p gpu-nvlink,dgx-spa
-#SBATCH --time=2-15:20:00
+#SBATCH --time=2-00:00:00
 #SBATCH --job-name=digitala_fi
 #SBATCH --mem=10G
 #SBATCH --output=output.out
@@ -12,11 +12,6 @@ module load anaconda
 module load cuda 
 source activate w2v2
 
-# torchrun --nproc_per_node=1 finetune.py --lang=fi
 srun python -u finetune.py --lang=fi
 
-# watch -n 5 nvidia-smi >> gpu_usage.txt
-# srun python -u finetune.py --lang=fi
-
-# srun python -u -m torch.distributed.launch \
-    # --nproc_per_node 4 finetune.py --lang=fi
+# torchrun --nproc_per_node=1 finetune.py --lang=fi
