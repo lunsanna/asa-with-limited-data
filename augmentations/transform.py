74f160e16b76abc61e7eeaaa6f834a5319530c89
Sanna Lun <tin.lun@aalto.fi>
2023-07-12 13:04:44 +0300

    Add additive noise transformation

--- augmentations/transform.py
+++ augmentations/transform.py
@@ -1,10 +1,13 @@
 from WavAugment import augment
 from datasets import Dataset, concatenate_datasets
 from functools import partial 
+from random import choice
 import logging
 import time 
 import numpy as np
 import copy
+import glob
+import torchaudio
 
 from helper import print_time
 
@@ -22,12 +25,13 @@ def time_masking(data_args: Dict[str, Any],
     new_example = copy.deepcopy(example)
     # (seq_len) -> (1, seq_len) expected by WavAugment
     # Otherwise all speech are 1D arrays
-    speech: Tensor = new_example["speech"].unsqueeze(dim=0) 
+    speech: Tensor = new_example["speech"]
+    speech = speech.unsqueeze(dim=0) if speech.ndim == 1 else speech
 
     # get hyperparams
-    max_mask_proportion: float = transform_args["max_mask_proportion"]
-    max_mask_size: float = transform_args["max_mask_size"]
-    sampling_rate = data_args["target_feature_extractor_sampling_rate"]
+    max_mask_proportion: float = transform_args.get("max_mask_proportion")
+    max_mask_size: float = transform_args.get("max_mask_size")
+    sampling_rate = data_args.get("target_feature_extractor_sampling_rate")
     
     assert max_mask_proportion < 1 and max_mask_proportion > 0, f"Masking proportion should be positive and < 1, got {max_mask_proportion}"
     assert speech.ndim == 2, f"Expect 1-dim speech signal, got {speech.ndim-1}. Please ensure batched=False"
@@ -54,27 +58,104 @@ def pitch_shift(data_args: Dict[str, Any],
                 example: Dict[str, Any]) -> Dict[str, Any]:
     
     new_example = copy.deepcopy(example)
-    speech: Tensor = new_example["speech"].unsqueeze(dim=0)
+    speech: Tensor = new_example["speech"]
+    speech = speech.unsqueeze(dim=0) if speech.ndim == 1 else speech
 
-    # Get hyperparams
-    sigma: int = transform_args["sigma"]
-    sampling_rate = data_args["target_feature_extractor_sampling_rate"]
-    assert sigma, f"Sigma value must be set to use pitch shift"
+    # Get hyperparams from config.yml
+    do_gaussian: bool = transform_args.get("gaussian")
+    sigma: int = transform_args.get("sigma")
+    range: int = transform_args.get("range")
+    sampling_rate = data_args.get("target_feature_extractor_sampling_rate")
+    assert sampling_rate and (do_gaussian is not None) and (sigma if do_gaussian else range)
 
-    effect_chain = augment.EffectChain() \
-        .pitch(lambda: np.random.normal(0, sigma)) \
-        .rate(sampling_rate)
+    def random_pitch_shift():
+        return np.random.normal(0, sigma) if do_gaussian else np.random.uniform(-range, range)
 
-    src_info = {'rate': sampling_rate}
-    target_info = {'rate': sampling_rate}
-    transformed_speech = effect_chain.apply(speech, src_info, target_info)
+    transformed_speech = augment.EffectChain() \
+        .pitch(random_pitch_shift) \
+        .rate(sampling_rate) \
+        .apply(speech, {'rate': sampling_rate}, {'rate': sampling_rate})
+
     new_example["speech"] = transformed_speech.squeeze()
     
     return new_example 
 
+def reverberation(data_args: Dict[str, Any], 
+                  transform_args: Dict[str, Any], 
+                  example: Dict[str, Any]) -> Dict[str, Any]:
+    # create a new copy of the example instead of mutating the original example
+    new_example = copy.deepcopy(example)
+    speech: Tensor = new_example["speech"]
+    speech = speech.unsqueeze(dim=0) if speech.ndim == 1 else speech
+
+    # get hyperparams from config.yml
+    sampling_rate = data_args.get("target_feature_extractor_sampling_rate")
+    room_size_sigma = transform_args.get("room_size_sigma")
+    assert sampling_rate and room_size_sigma
+
+    def random_room_size():
+        r = np.random.normal(0,room_size_sigma)
+        return min(abs(r), 100)
+    
+    transformed_speech = augment.EffectChain()\
+        .reverb(50, 50, random_room_size) \
+        .channels(1) \
+        .apply(speech, {'rate': sampling_rate}, {'rate': sampling_rate})
+    
+    new_example["speech"] = transformed_speech.squeeze()
+
+    return new_example
+
+def additive_noise(data_args: Dict[str, Any], 
+                  transform_args: Dict[str, Any], 
+                  example: Dict[str, Any]) -> Dict[str, Any]:
+    # copy example 
+    new_example = copy.deepcopy(example)
+    speech: Tensor = new_example["speech"] # (seq_len, ), used by Wav2Vec2
+    speech = speech if speech.ndim == 2 else speech.unsqueeze(dim=0) # (1, seq_len), expected by WavAugment
+    snr_low: int = transform_args["snr_low"]
+    snr_high: int = transform_args["snr_high"]
+    sampling_rate: int = data_args["target_feature_extractor_sampling_rate"]
+    assert speech.ndim == 2 and speech.size(0) == 1
+
+    # choose random noise sample from Musan
+    noise_dir = transform_args["noise_dir"]
+    noise_paths = glob.glob(f"{noise_dir}/*")
+    noise_paths = noise_paths if len(noise_paths) > 0 else glob.glob(f"../{noise_dir}/*")
+    random_noise_path: str = choice(noise_paths)
+    noise, noise_sr = torchaudio.load(random_noise_path)
+    if noise_sr != sampling_rate:
+        resampler = torchaudio.transforms.Resample(noise_sr, sampling_rate)
+        noise = resampler(noise)
+
+    # adjust length to match speech
+    speech_len: int = speech.size(1)
+    noise_len: int = noise.size(1)
+    assert speech_len > 0 and noise_len > 0, f"Speech: {speech_len}, noise: {noise_len}"
+
+    if speech_len > noise_len:
+        repeat_factor: int = speech_len//noise_len + 1
+        noise = noise.repeat(1,repeat_factor)
+    noise = noise[:,:speech_len]
+    assert speech.size() == noise.size()
+
+    # add noise 
+    snr = np.random.randint(snr_low, snr_high+1)
+    snr_linear = 10**(snr/10)
+    speech_power = (speech**2).mean()
+    noise_power = (noise**2).mean()
+    noise_factor = np.sqrt(speech_power/(noise_power*snr_linear))
+    new_example["speech"] = speech + noise_factor * noise
+    # adjust dim 
+    new_example["speech"] = new_example["speech"].squeeze()
+
+    return new_example
+
 transform_dict = {
     "time_masking": time_masking, 
-    "pitch_shift": pitch_shift
+    "pitch_shift": pitch_shift, 
+    "reverberation": reverberation,
+    "additive_noise": additive_noise,
 }
 
 def apply_tranformations(train_dataset: Dataset,  
@@ -89,7 +170,7 @@ def apply_tranformations(train_dataset: Dataset,
     augmented_train_dataset = train_dataset.map(transform_partial)
     
     if augment_args["copy"]:
-        train_dataset = concatenate_datasets([train_dataset, augmented_train_dataset]).shuffle(seed=42)
+        train_dataset = concatenate_datasets([train_dataset, augmented_train_dataset]).shuffle()
     else:
         train_dataset = augmented_train_dataset
 
