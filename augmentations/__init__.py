5b93d684785184655417b9766e475e5461519066
Sanna Lun <tin.lun@aalto.fi>
2023-07-17 13:36:35 +0300

    implement all classic augmentation methods

--- augmentations/__init__.py
+++ augmentations/__init__.py
@@ -1,6 +1,21 @@
-from .transform import apply_tranformations, transform_names
+from .transform import (apply_tranformations, 
+                        time_masking,
+                        pitch_shift, 
+                        reverberation, 
+                        additive_noise,
+                        band_reject,
+                        tempo_perturbation,
+                        transform_dict,
+                        transform_names)
 
 __all__ = [
     'apply_tranformations', 
+    'time_masking',
+    'pitch_shift', 
+    'reverberation', 
+    'additive_noise',
+    'band_reject',
+    'tempo_perturbation',
+    'transform_dict',
     'transform_names'
 ]
--- augmentations/transform.py
+++ augmentations/transform.py
@@ -13,7 +13,7 @@ from helper import print_time
 
 # for typing 
 from torch import Tensor
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 logger = logging.getLogger(__name__)
 
@@ -31,25 +31,26 @@ def time_masking(data_args: Dict[str, Any],
     # get hyperparams
     max_mask_proportion: float = transform_args.get("max_mask_proportion")
     max_mask_size: float = transform_args.get("max_mask_size")
+    min_mask_size: float = transform_args.get("min_mask_size")
     sampling_rate = data_args.get("target_feature_extractor_sampling_rate")
     
     assert max_mask_proportion < 1 and max_mask_proportion > 0, f"Masking proportion should be positive and < 1, got {max_mask_proportion}"
     assert speech.ndim == 2, f"Expect 1-dim speech signal, got {speech.ndim-1}. Please ensure batched=False"
 
-    max_frames = sampling_rate*max_mask_size  # max frames per mask
-    effect_chain = augment.EffectChain().time_dropout(max_frames=max_frames)
+    max_frames = int(sampling_rate*max_mask_size)  # max frames per mask
+    min_frames = int(sampling_rate*min_mask_size) # min frames per mask
 
     # num of masks apply proportional to the chosen max mask percentage
     n_masks = int(max_mask_proportion*speech.size(1)//max_frames)
 
     # apply masking n_masks times
-    src_info = {'rate': sampling_rate} # resampled to target sr in earlier steps
-    target_info = {'rate': sampling_rate}
-    transformed_speech = speech.clone()
-    for _ in range(n_masks):
-        transformed_speech = effect_chain.apply(transformed_speech, src_info, target_info)
+    mask_lengths = np.random.randint(min_frames, max_frames, n_masks)
+    for length in mask_lengths:
+        start = np.random.randint(0, speech.size(1) - length)
+        end = start + length
+        speech[:,start:end,...].zero_()
 
-    new_example["speech"] = transformed_speech.squeeze()  # 1D speech array
+    new_example["speech"] = speech.squeeze()  # 1D speech array
     return new_example
 
 
@@ -62,14 +63,15 @@ def pitch_shift(data_args: Dict[str, Any],
     speech = speech.unsqueeze(dim=0) if speech.ndim == 1 else speech
 
     # Get hyperparams from config.yml
-    do_gaussian: bool = transform_args.get("gaussian")
     sigma: int = transform_args.get("sigma")
-    range: int = transform_args.get("range")
     sampling_rate = data_args.get("target_feature_extractor_sampling_rate")
-    assert sampling_rate and (do_gaussian is not None) and (sigma if do_gaussian else range)
+    assert sampling_rate 
 
     def random_pitch_shift():
-        return np.random.normal(0, sigma) if do_gaussian else np.random.uniform(-range, range)
+        shift = 0 
+        while shift == 0:
+            shift = np.random.normal(0, sigma)
+        return shift 
 
     transformed_speech = augment.EffectChain() \
         .pitch(random_pitch_shift) \
@@ -120,7 +122,7 @@ def additive_noise(data_args: Dict[str, Any],
 
     # choose random noise sample from Musan
     noise_dir = transform_args["noise_dir"]
-    noise_paths = glob.glob(f"{noise_dir}/*")
+    noise_paths = glob.glob(f"{noise_dir}/*.wav")
     noise_paths = noise_paths if len(noise_paths) > 0 else glob.glob(f"../{noise_dir}/*")
     random_noise_path: str = choice(noise_paths)
     noise, noise_sr = torchaudio.load(random_noise_path)
@@ -151,17 +153,81 @@ def additive_noise(data_args: Dict[str, Any],
 
     return new_example
 
+def band_reject(data_args: Dict[str, Any], 
+                transform_args: Dict[str, Any], 
+                example: Dict[str, Any]) -> Dict[str, Any]:
+    
+    new_example = copy.deepcopy(example)
+    speech = new_example["speech"]
+    speech = speech if speech.ndim == 2 else speech.unsqueeze(dim=0)
+    assert speech.ndim == 2 and speech.size(0) == 1
+    max_mask_proportion = transform_args["max_mask_proportion"]
+    assert max_mask_proportion > 0 and max_mask_proportion < 1
+    sampling_rate: int = data_args["target_feature_extractor_sampling_rate"]
+    
+    mask_width = transform_args["mask_width"] # constant
+    n_mask = int((sampling_rate/2)*max_mask_proportion//transform_args["mask_width"])
+
+    # the following two lines are taken from Introduction to Speech Processing by Tom Bäckström et el
+    # https://speechprocessingbook.aalto.fi/Representations/Melcepstrum.html?highlight=mel
+    def freq2mel(f): return 2595*np.log10(1 + (f/700))
+    def mel2freq(m): return 700*(10**(m/2595) - 1)
+    
+    mel_upper = freq2mel(sampling_rate/2 - mask_width)
+    starts_mel = np.random.uniform(0, mel_upper, n_mask)
+    starts_f = mel2freq(starts_mel).astype(int)
+    ends = starts_f + mask_width
+    bands_to_reject = list(zip(starts_f, ends))
+
+    effects = [["sinc", "-a", "120", f"{f2}-{f1}"] for f1, f2 in bands_to_reject]
+    augmented_speech, sr = torchaudio.sox_effects.apply_effects_tensor(
+        speech, 
+        sampling_rate, 
+        effects, 
+        channels_first=True
+    )
+    assert sr == sampling_rate
+    new_example["speech"] = augmented_speech.squeeze()
+
+    return new_example
+
+
+def tempo_perturbation(data_args: Dict[str, Any], 
+                       transform_args: Dict[str, Any], 
+                       example: Dict[str, Any]) -> Dict[str, Any]:
+    new_example = copy.deepcopy(example)
+    speech: Tensor = new_example["speech"]
+    speech = speech if speech.ndim==2 else speech.unsqueeze(dim=0)
+    perturb_factors: List[int] = transform_args["perturbation_factors"]
+    perturb_factor: int = choice(perturb_factors)
+    sampling_rate: int = data_args.get("target_feature_extractor_sampling_rate")
+
+    assert speech.ndim==2 and sampling_rate
+
+    # apply tempo perturbation
+    effects = [["tempo", str(perturb_factor)]]
+    speech, sr = torchaudio.sox_effects.apply_effects_tensor(
+        speech, sampling_rate, effects, channels_first=True
+    )
+    assert sampling_rate==sr
+
+    new_example["speech"] = speech.squeeze()
+    return new_example
+    
+
 transform_dict = {
     "time_masking": time_masking, 
     "pitch_shift": pitch_shift, 
     "reverberation": reverberation,
     "additive_noise": additive_noise,
+    "band_reject": band_reject, 
+    "tempo_perturbation": tempo_perturbation
 }
 
 def apply_tranformations(train_dataset: Dataset,  
                          data_args: Dict[str, Any], 
                          augment_args: Dict[str, Any], 
-                         augment_name: str):
+                         augment_name: str) -> Dataset:
     start = time.time()
     transform = transform_dict[augment_name]
     transform_args = augment_args[augment_name] # params for the chosen transformation
