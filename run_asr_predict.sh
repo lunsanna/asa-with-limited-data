3015f4e2ac890c977f6756b4a850ae44c858c33c
Sanna Lun <tin.lun@aalto.fi>
2023-07-31 19:16:27 +0300

    initial commit

---
+++ run_asr_predict.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=0-00:15:00
+#SBATCH --job-name=asr_no_augment
+#SBATCH --mem=5G
+#SBATCH --array=0-3
+#SBATCH --output=asr_output_%a.out
+#SBATCH --error=asr_errors_%a.err
+
+module load anaconda
+module load cuda 
+source activate w2v2
+
+srun python -u ../../run_predict.py \
+--fold=$SLURM_ARRAY_TASK_ID \
+--partial_model_path=output_fold_
