5d7f7b618687105a33845e621f1b1f3422831615
Sanna Lun <tin.lun@aalto.fi>
2023-08-01 10:25:58 +0300

    minor updates

--- run_finetune.sh
+++ run_finetune.sh
@@ -2,12 +2,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH -p gpu-nvlink,dgx-spa
-#SBATCH --time=0-10:00:00
+#SBATCH --time=0-09:00:00
 #SBATCH --job-name=no_augment
-#SBATCH --mem=10G
+#SBATCH --mem=30G
 #SBATCH --array=0-3
-#SBATCH --output=ex0_output_%a.out
-#SBATCH --error=ex0_errors_%a.err
+#SBATCH --output=output_%a.out
+#SBATCH --error=errors_%a.err
 
 module load anaconda
 module load cuda 
